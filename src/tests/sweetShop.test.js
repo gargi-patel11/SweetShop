bd22c7a9d5876a0c79e34e180f864b2176549527
gargi-patel11 <gargipatel2211@gmail.com>
2025-07-17 02:03:21 +0530

    add a test case for deleting Sweet by ID

--- src/tests/sweetShop.test.js
+++ src/tests/sweetShop.test.js
@@ -1,15 +1,27 @@
-const Sweet = require("../models/Sweet") ;
-const SweetShop = require("../models/sweetShop") ; 
+const Sweet = require("../models/Sweet");
+const SweetShop = require("../models/sweetShop");
 
-describe("SweetShop class" , ()=>{
-    let shop  ; 
-    shop = new SweetShop() ; 
+describe("SweetShop class", () => {
+  let shop;
+  beforeEach(() => {
+    shop = new SweetShop();
+  });
 
-    test('should add a Sweet ', () => {
-        const sweet = new Sweet(1001, 'Kaju Katli', 'Nut-Based', 50, 20);
-        shop.addSweet(sweet);
+  test("should add a Sweet ", () => {
+    const sweet = new Sweet(1001, "Kaju Katli", "Nut-Based", 50, 20);
+    shop.addSweet(sweet);
 
-        expect(shop.viewSweets()).toContainEqual(sweet);
-    })
-    
-})
+    // expect(shop.viewSweets()).toContainEqual(sweet);
+  });
+
+  test("should delete a sweet by ID", () => {
+    const sweet = new Sweet(1001, "Kaju Katli", "Nut-Based", 50, 20);
+    shop.addSweet(sweet);
+
+    shop.deleteSweet(1001);
+
+    expect(shop.viewSweets()).not.toContainEqual(sweet);
+  });
+
+  
+});
